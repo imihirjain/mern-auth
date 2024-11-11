6b4ceb71f9e85cbdfe55df18773eb125e83718b8
imihirjain <jmanav251@gmail.com>
2024-11-11 17:22:40 +0530

    create database, auth route, user schema, auth controller

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("SignUp route");
+};
+
+export const login = async (req, res) => {
+  res.send("SignUp route");
+};
+
+export const logout = async (req, res) => {
+  res.send("SignUp route");
+};
