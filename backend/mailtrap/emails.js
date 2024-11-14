499178b5d2e148dc8816e6ae7be019741db3cab9
imihirjain <jmanav251@gmail.com>
2024-11-14 19:36:20 +0530

    create verify email endpoint, template and verify the email successfully

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -19,3 +19,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error seding verification email ${error}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const res = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "4474adf9-7913-4c8c-a190-9ce07cb23856",
+      template_variables: {
+        name: name,
+        company_info_name: "V A U L T I F Y",
+      },
+    });
+
+    console.log("Welcome Email sent successfully", res);
+  } catch (error) {
+    console.error("Error sending welcome Email", error);
+    throw new Error(`Error sending welcome email ${error}`);
+  }
+};
