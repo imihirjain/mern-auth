d1eb8c2f87e9ad3752669d7122923ef0108f2cb6
imihirjain <jmanav251@gmail.com>
2024-12-03 14:50:23 +0530

    some minor changes in package json and index.js

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,26 @@ import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allow use to parse the incomingn requests:- req.body
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
